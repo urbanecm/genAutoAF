cf5b45f7479635d1e654fba7adde52dc2d8514cb
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-09-26 09:35:21 +0200

    Use newlines

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo 'select page_title from templatelinks join page on page_id=tl_from where tl_title="Sdílená_IP_škola" and page_title not like "%:%" and page_namespace=3' | sql cswiki | sed 1d > ips.txt; php script.php ips.txt; cp re.txt ~/tmpPublic/re.txt; echo https://tools.wmflabs.org/urbanecmbot/test/re.txt
+echo 'select page_title from templatelinks join page on page_id=tl_from where tl_title="Sdílená_IP_škola" and page_title not like "%:%" and page_namespace=3' | sql cswiki | sed 1d > ips.txt
+php script.php ips.txt
+cp re.txt ~/tmpPublic/re.txt
+echo https://tools.wmflabs.org/urbanecmbot/test/re.txt
