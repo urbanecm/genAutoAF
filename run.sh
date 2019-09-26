72b0cce93a76886a117327cb2d8353c9327d1d6a
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-09-26 09:46:04 +0200

    Do splitting in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 echo 'select page_title from templatelinks join page on page_id=tl_from where tl_title="Sdílená_IP_škola" and page_title not like "%:%" and page_namespace=3' | sql cswiki | sed 1d > ips.txt
-php script.php ips.txt
-cp re.txt ~/tmpPublic/re.txt
-echo https://tools.wmflabs.org/urbanecmbot/test/re.txt
+split -l 2500 ips.txt ips-
+for ips in ips-*; do
+	re=$(echo $ips | sed 's/ips/re/g')
+	php script.php $ips $re
+done
+rm ips*
+for re in re-*; do
+	cp $re ~/tmpPublic/$re.txt
+	echo https://tools.wmflabs.org/urbanecmbot/test/$re.txt
+done
+rm re*
